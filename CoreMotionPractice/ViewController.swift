f3620580fb9d4bca3779e3b6b770a8e852a55258
yeon32 <32jyeon@gamil.com>
2022-09-22 15:28:02 +0900

    Initial Commit

---
+++ CoreMotionPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreMotionPractice
+//
+//  Created by 김정연 on 2022/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
